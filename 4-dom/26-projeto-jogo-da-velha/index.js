02082c64c313a025135e5d09b3857afc7eab1dfe
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-07-03 16:34:52 -0300

    bug fix jogador da vez e adicionando titulo empate

--- 4-dom/26-projeto-jogo-da-velha/index.js
+++ 4-dom/26-projeto-jogo-da-velha/index.js
@@ -13,44 +13,48 @@ const playerTurn = document.querySelector("h2");
 
 document.querySelectorAll(".availablePlay").forEach(function (ticTacToeBtn){
     ticTacToeBtn.addEventListener("click", function jogada(){
-        if(tagJogada){
-            playerTurn.innerText = `É a sua vez, ${playerOne.value}!`;
-            console.log(playerOne);
-            ticTacToeBtn.innerText = "O";
-            //ticTacToeBtn.disabled = true;
-            ticTacToeBtn.classList.remove("availablePlay");
-            ticTacToeBtn.classList.add("unavailablePlay");
-            ticTacToeBtn.removeEventListener("click", jogada);
-            tagJogada = false;
-            matrizDoJogo[parseInt(ticTacToeBtn.dataset.value[0])][parseInt(ticTacToeBtn.dataset.value[2])] = "O";
-            //console.table(matrizDoJogo);
-            const isPartida = verificaTabuleiro();
-            contagemEmpate++;
-            if (isPartida) {
-                alert(`Parabéns ${playerOne.value}, você venceu!`);
-            }
-            if(contagemEmpate===9){
-                alert("jogo empatado!");
+        if(playerOne.value != "" && playerTwo.value!= "") {
+            if(tagJogada){
+                playerTurn.innerText = `É a sua vez, ${playerOne.value}!`;
+                console.log(playerOne);
+                ticTacToeBtn.innerText = "O";
+                //ticTacToeBtn.disabled = true;
+                ticTacToeBtn.classList.remove("availablePlay");
+                ticTacToeBtn.classList.add("unavailablePlay");
+                ticTacToeBtn.removeEventListener("click", jogada);
+                tagJogada = false;
+                matrizDoJogo[parseInt(ticTacToeBtn.dataset.value[0])][parseInt(ticTacToeBtn.dataset.value[2])] = "O";
+                //console.table(matrizDoJogo);
+                const isPartida = verificaTabuleiro();
                 contagemEmpate++;
+                if (isPartida) {
+                    alert(`Parabéns ${playerOne.value}, você venceu!`);
+                }
+                if(contagemEmpate===9){
+                    playerTurn.innerText = "Jogo empatado!";
+                    contagemEmpate++;
+                }
+            }else{
+                playerTurn.innerText = `É a sua vez, ${playerTwo.value}!`;
+                ticTacToeBtn.innerText = "X";
+                ticTacToeBtn.classList.remove("availablePlay");
+                ticTacToeBtn.classList.add("unavailablePlay");
+                ticTacToeBtn.removeEventListener("click", jogada);
+                //ticTacToeBtn.disabled = true;
+                tagJogada = true;
+                matrizDoJogo[parseInt(ticTacToeBtn.dataset.value[0])][parseInt(ticTacToeBtn.dataset.value[2])] = "X";
+                const isPartida = verificaTabuleiro();
+                contagemEmpate++;
+                if (isPartida) {
+                    alert(`Parabéns ${playerTwo.value}, você venceu!`);
+                }
+                if(contagemEmpate===9){
+                    playerTurn.innerText = "Jogo empatado!";
+                }
+                //console.table(matrizDoJogo);
             }
         }else{
-            playerTurn.innerText = `É a sua vez, ${playerTwo.value}!`;
-            ticTacToeBtn.innerText = "X";
-            ticTacToeBtn.classList.remove("availablePlay");
-            ticTacToeBtn.classList.add("unavailablePlay");
-            ticTacToeBtn.removeEventListener("click", jogada);
-            //ticTacToeBtn.disabled = true;
-            tagJogada = true;
-            matrizDoJogo[parseInt(ticTacToeBtn.dataset.value[0])][parseInt(ticTacToeBtn.dataset.value[2])] = "X";
-            const isPartida = verificaTabuleiro();
-            contagemEmpate++;
-            if (isPartida) {
-                alert(`Parabéns ${playerTwo.value}, você venceu!`);
-            }
-            if(contagemEmpate===9){
-                alert("jogo empatado!");
-            }
-            //console.table(matrizDoJogo);
+            alert("Por favor insira o nome dos jogadores!");
         }
     })
 });
