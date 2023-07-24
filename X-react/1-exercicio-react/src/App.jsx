d473e1aa7869a430fc8556f26fadbd8211868c55
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-07-23 22:21:04 -0300

    commit inicial e resolução do exercicio

---
+++ X-react/1-exercicio-react/src/App.jsx
@@ -0,0 +1,33 @@
+export default function App() {
+
+  return (
+    <>
+      <div>
+        <img src="https://onebitcode.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Fea7d49f7-85ff-4dc5-96bf-7101fd3e1565%2Freact.png?table=block&id=8bc24587-6336-4b7b-9fe0-1a5d63bccbc8&spaceId=6e5271d8-2f68-42f5-aa75-5978bbff47fa&width=270&userId=&cache=v2" alt="logo do react" />
+      </div>
+      <h1>React</h1>
+      <p>A biblioteca para interfaces de usuario web e nativas.</p>
+      <div>
+        <button>Aprenda React</button>
+        <button>Referência da API</button>
+      </div>
+      <hr />
+      <h1>Crie Interfaces de usuario de componentes</h1>
+      <p>React permite que voce construa interfaces de usuario a partir de pedacos indivisiveis chamados componentes.</p>
+      <hr />
+      <h1>Escreva componentes com codigos e marcação</h1>
+      <p>Componentes React são funcoes javascript. A sintaxe de marcação é chamada de JSX. É uma extensão da sintaxe Javascript popularizada pelo React.</p>
+      <hr />
+      <h1>Próximos passos</h1>
+      <ol>
+        <li>Uso de dados dinamicos no JSX.</li>
+        <li>Criação de novos componentes.</li>
+        <li>Estilização de componentes.</li>
+        <li>Reutilização de componenetes.</li>
+        <li>Uso de props e children.</li>
+        <li>Uso de eventos do javascript</li>
+      </ol>
+      
+    </>
+  )
+}
