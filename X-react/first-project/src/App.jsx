d11612f28c6eefc2a819916ae9794d2463ded273
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-07-23 22:08:01 -0300

    limpando o projeto

--- X-react/first-project/src/App.jsx
+++ X-react/first-project/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Olá Mundo!</h1>
+      <p>Hey ya! Primeiro projeto do curso OneBit Code usando React e Vite!</p>
+      <p>Next Steps:</p>
+      <ul>
+        <li>Praticar com exercicios</li>
+        <li>Aprender a usar dados dinamicos</li>
+        <li>Criar novos componentes</li>
+        <li>Estilizar os componentes</li>
+        <li>Reutilizar componentes</li>
+      </ul>
     </>
   )
 }
-
-export default App
