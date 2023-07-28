ba5111375ae615991e78aece5301e15ea2893ab3
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-07-28 19:35:56 -0300

    aula props react

--- X-react/3-card-component/src/App.jsx
+++ X-react/3-card-component/src/App.jsx
@@ -1,12 +1,15 @@
 import Card from "./components/Card"
+import episode4Poster from "./assets/sw-poster.jpg"
+import episode5Poster from "./assets/esb-poster.jpg"
+import episode6Poster from "./assets/rotj-poster.jpg"
 
 function App() {
 
   return (
     <div>
-      <Card/>
-      {/* <Card/> */}
-      {/* <Card/> */}
+      <Card src={episode4Poster} title="Pôster: Star Wars (1977)"/>
+      <Card src={episode5Poster} title="Pôster: Empire Strikes Back(1980)"/>
+      <Card src={episode6Poster} title="Pôster: Return of the Jedi(1983)"/>
     </div>
   )
 }
--- X-react/3-card-component/src/components/Card/index.jsx
+++ X-react/3-card-component/src/components/Card/index.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable react/prop-types */
 import styles from "./index.module.css"
 import Button from "../Button"
 
-export default function Card(){
+export default function Card({src, title}){
     return (
         <div className={styles.container}>
-            <img className={styles.poster} src="https://i.pinimg.com/originals/ba/94/64/ba9464145eba8762f6286a3c8387c951.jpg" alt="poster star wars " />
+            <img className={styles.poster} src={src} alt="poster star wars " />
             <div>
-                <h2>Poster: Star Wars (1977)</h2>
+                <h2>{title} (1977)</h2>
                 <p>Um poster decorativo epico do filme Star Warsm com moldura de MDF e tamanho A3. Uma otima recordacao de um dos mais iconicos filmes de todos os tempos. Este classico poster trara aventura, nostalgia e a magia de Star Wars para qualquer lugar que voce decidir pendurar. Nao perca a chance de adicionar essa linda memoria ao seu acervo!</p>
                 <Button />
             </div>
