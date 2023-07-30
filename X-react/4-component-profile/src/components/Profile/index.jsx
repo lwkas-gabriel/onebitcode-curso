64c38df9de5afbf19bd71b937175fb628e6d64b0
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-07-30 15:33:09 -0300

    refatorando exercicio

--- X-react/4-component-profile/src/components/Profile/index.jsx
+++ X-react/4-component-profile/src/components/Profile/index.jsx
@@ -6,16 +6,20 @@ export default function Profile(props){
     return (
         <div className={styles.container}>
             <img src="https://github.com/lwkas-gabriel.png" alt="foto do profissional" />
-            <h2>{props.name}</h2>
-            <hr />
-            <p>{props.position}</p>
-            <hr />
-            <p>{props.phone}</p>
-            <hr />
-            <p>{props.email}</p>
-            <hr />
+            <div>
+                <h2>{props.name}</h2>
+            </div>
+            <div>
+                <p>{props.position}</p>
+            </div>
+            <div>
+                <p>{props.phone}</p>
+            </div>
+            <div>
+                <p>{props.email}</p>
+            </div>
             <Button nameBtn="Github" />
-            <Button nameBtn="Linkedin" />
+            <Button nameBtn="Linkedin" /> 
             <Button nameBtn="Twitter" />
         </div>
     );
