efc1be622c1f8cf7e2d1723ba6dd2ce40a28d0fb
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-08-03 17:26:46 -0300

    controlando inputs com react

--- X-react/6-gerador-senhas/src/App.jsx
+++ X-react/6-gerador-senhas/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
   function handleNewRandomPasswordGenerate(){
     let senha = "";
     const chars = "0123456789abcdefghijklmnopqrstuvwxyz_!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const tamanhoSenha = 12;
+    // const tamanhoSenha = 12;
 
-    for (let i = 0; i <= tamanhoSenha; i++){
+    for (let i = 0; i < tamanhoSenha; i++){
       let posicaoAleatoria = Math.floor(Math.random() * chars.length);
       senha += chars[posicaoAleatoria]
     }
@@ -23,10 +23,22 @@ function App() {
 
   const [estadoCopiado, setEstadoCopiado] = useState("Copiar");
   const [senha, setSenha] = useState("");
+  const [tamanhoSenha, setTamanhoSenha] = useState(12);
+
   return (
     <>
       <h1>Gerador de Senhas</h1>
       <div className="card">
+        <div>
+          <label htmlFor="passwordSize">Tamanho: </label>
+          <input
+            id="passwordSize"
+            type="number"
+            min={1}
+            value={tamanhoSenha}
+            onChange={(ev) => setTamanhoSenha(ev.target.value)}
+          />
+        </div>
         <button onClick={handleNewRandomPasswordGenerate}>
           Gerar!
         </button>
