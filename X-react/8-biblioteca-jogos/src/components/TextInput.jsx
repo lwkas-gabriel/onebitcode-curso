8e6bc916dd51c512ac5591f3b9e08f18609774ba
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-08-10 18:01:47 -0300

    refatorando: componentizando e criando novo hook

---
+++ X-react/8-biblioteca-jogos/src/components/TextInput.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types"
+
+TextInput.propTypes = {
+    id: PropTypes.string,
+    value: PropTypes.string,
+    setValue: PropTypes.func,
+    label: PropTypes.string
+}
+
+export default function TextInput({id, label, value, setValue}){ 
+    return (
+        <div>
+            <label htmlFor={label}>{label}: </label>
+            <input
+                type="text"
+                name={id}
+                id={id}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
+        </div>
+    );
+}
