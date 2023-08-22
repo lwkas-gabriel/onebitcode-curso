a122a0d32a52706e6d7dca5d3fe4c09c9a14d28d
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-08-22 17:39:23 -0300

    commit e estilizações iniciais

---
+++ X-react/9-feed-comentarios/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <div>
+      <form action="">
+        <h1>Seção de comentários</h1>
+        <label htmlFor="">Email</label>
+        <input type="email" />
+        <label htmlFor="">Comentário</label>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button type='submit'>Submit</button>
+        <hr />
+      </form>
+    </div>
+  )
+}
+
+export default App
