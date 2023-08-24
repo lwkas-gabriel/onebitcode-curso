55dc2aadf7ffb9518ce2392d5322f06fc78a8799
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-08-24 19:11:47 -0300

    componentizando: Comment e TextInput

---
+++ X-react/9-feed-comentarios/src/components/Comment/index.jsx
@@ -0,0 +1,9 @@
+export function Comment(){
+    return (
+        <div>
+            <strong>email@email</strong>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, id.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, excepturi.</p>
+        </div>
+    )
+}
--- X-react/9-feed-comentarios/src/components/NewCommentForm/index.jsx
+++ X-react/9-feed-comentarios/src/components/NewCommentForm/index.jsx
@@ -1,14 +1,24 @@
 // eslint-disable-next-line no-unused-vars
 import styles from "./styles.module.css"
+import { TextInput } from "../TextInput"
+import { useState } from "react";
 
 export function NewCommentForm(){
+    const [email, setEmail] = useState("");
+    const [comment, setComment] = useState("");
     return (
         <form action="">
             <h1>Seção de comentários</h1>
-            <label htmlFor="">Email</label>
-            <input type="email" />
-            <label htmlFor="">Comentário</label>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <TextInput
+                id="email"
+                label="Email"
+                value={email}
+            />
+            <TextInput
+                id="comment"
+                label="Comentário"
+                value={comment}
+            />
             <button type='submit'>Submit</button>
         <hr />
       </form>
