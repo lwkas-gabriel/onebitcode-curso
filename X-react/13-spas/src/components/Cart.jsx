be6bc68754f3c9d36c5e832227f331a17b07d6c8
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-09-07 15:53:41 -0300

    criando os componentes iniciais

---
+++ X-react/13-spas/src/components/Cart.jsx
@@ -0,0 +1,15 @@
+export default function Cart(){
+    <section>
+        <h2>Carrinho</h2>
+        <p>Os produtos atualmente em seu carrinho.</p>
+        <ul>
+            <li>2 un. - Memória RAM DDR4v 8GB</li>
+            <li>1 un. - Processador Intel Core i3 12th Gen</li>
+            <li>1 un. - SSD Sata 240 GB</li>
+            <li>1 un. - Fonte ATX 500W</li>
+            <li>1 un. - Placa Mãe Intel LGA 1700</li>
+            <li>1 un. - Gabinete</li>
+        </ul>
+        <button>Finalizar compra</button>
+    </section>
+}
