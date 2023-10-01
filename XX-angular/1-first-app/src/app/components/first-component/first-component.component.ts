2e5303711f67b9b4bf94f91db2ea32859b3b3095
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-01 17:27:10 -0300

    criação do primeiro componente e ciclo de vida

---
+++ XX-angular/1-first-app/src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  constructor(){}
+
+  variable = 1;
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  varChange(){
+    this.variable++;
+  }
+}
