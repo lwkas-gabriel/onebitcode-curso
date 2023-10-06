f21f62fd9c9ce42360b9a788c0aec2d264358bd6
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-10-06 08:48:13 -0300

    criando home + search + about e rotas

--- XX-angular/2-healthy-recipes/src/app/app-routing.module.ts
+++ XX-angular/2-healthy-recipes/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { SearchComponent } from './components/pages/search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "/", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "search", component: SearchComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- XX-angular/2-healthy-recipes/src/app/app.module.ts
+++ XX-angular/2-healthy-recipes/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { SearchComponent } from './components/pages/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
