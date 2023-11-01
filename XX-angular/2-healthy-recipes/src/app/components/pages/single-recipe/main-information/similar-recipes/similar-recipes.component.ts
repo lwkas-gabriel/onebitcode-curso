78142a95c377fb214c25dd92a0f6cfda006e92c6
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-01 19:03:29 -0300

    criando componenetes de receita unica e similares

---
+++ XX-angular/2-healthy-recipes/src/app/components/pages/single-recipe/main-information/similar-recipes/similar-recipes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-similar-recipes',
+  templateUrl: './similar-recipes.component.html',
+  styleUrls: ['./similar-recipes.component.css']
+})
+export class SimilarRecipesComponent {
+
+}
