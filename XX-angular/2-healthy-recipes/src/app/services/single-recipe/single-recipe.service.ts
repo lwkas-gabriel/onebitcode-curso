752b7e417f9dd96a8001e42d279004ad1f29a740
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-01 19:09:05 -0300

    criando e implementando serviço de receita unica

---
+++ XX-angular/2-healthy-recipes/src/app/services/single-recipe/single-recipe.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SingleRecipeService {
+
+  constructor(private http: HttpClient) { }
+
+  takeRecipe(id: number){
+    const apiUrl = `https://api.spoonacular.com/recipes/${id}/information?apiKey=884df11ed8b8401b9a2582ff7198c7a8`
+
+    return this.http.get(apiUrl);
+  }
+}
