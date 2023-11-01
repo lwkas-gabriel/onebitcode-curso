ebc5fdf4c156f24670d9ad6aa0f9e7e3903495db
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-01 19:19:56 -0300

    implementando o uso do serviço

--- XX-angular/2-healthy-recipes/src/app/components/pages/single-recipe/main-information/main-information.component.ts
+++ XX-angular/2-healthy-recipes/src/app/components/pages/single-recipe/main-information/main-information.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { finalize } from 'rxjs';
+import { SingleRecipeService } from 'src/app/services/single-recipe/single-recipe.service';
 
 @Component({
   selector: 'app-main-information',
@@ -6,5 +9,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-information.component.css']
 })
 export class MainInformationComponent {
+  public loading: boolean = true;
+  public recipeId: number = 1;
+  public food:any = [];
+
+  constructor(private service: SingleRecipeService, private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void{
+    // pega o id da direto da rota e joga na variavel de Id já inicializada
+    this.getRecipeId();
+    // usa o id ja pego e retorna as infos da receita desejada...
+    this.takeRecipe(this.recipeId);
+  }
+
+  getRecipeId(){
+    this.route.queryParams.subscribe((queryParams: any)=>{
+      //a linha abaixo pego id da rota
+      this.recipeId = queryParams("id");
+    });
+  }
+
+  takeRecipe(id: number){
+    this.service
+    .takeRecipe(id)
+    .pipe(
+      finalize(()=> {
+        this.loading = false;
+      })
+      ).subscribe((data: any) => {
+        this.food = data;
+      });
+  }
 
 }
