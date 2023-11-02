ddfe7efe82e4f8c6f2085eb50652ca494aa88b90
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-02 16:00:27 -0300

    implementando uso do serviço de search

--- XX-angular/2-healthy-recipes/src/app/components/pages/search/search.component.ts
+++ XX-angular/2-healthy-recipes/src/app/components/pages/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { finalize } from 'rxjs';
+import { SearchService } from 'src/app/services/search/search.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+  public loading: boolean = false;
+  public query: string = "";
+  public foods: any = [];
 
+  constructor(private service: SearchService){}
+
+  getSearch(){
+    this.service.search(this.query).pipe(finalize(()=>{
+      this.loading = false;
+    })
+    ).subscribe((data: any)=>{
+      this.foods = data.results;
+    });
+  }
 }
